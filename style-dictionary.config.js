35d9f779edeec9f2dc9a7b1b61d9757702a5bc8a
rafalwlodarek <61891339+rafalwlodarek@users.noreply.github.com>
2025-02-14 01:24:19 +0100

    Update and rename config.json to style-dictionary.config.js

---
+++ style-dictionary.config.js
@@ -0,0 +1,50 @@
+const StyleDictionary = require("style-dictionary");
+
+StyleDictionary.registerTransform({
+  name: "custom/flatten",
+  type: "attribute",
+  matcher: () => true,
+  transformer: (token) => ({
+    type: token.$type,
+    value: token.$value,
+  }),
+});
+
+module.exports = {
+  source: ["tokens/**/*.json"],
+  platforms: {
+    scss: {
+      transformGroup: "scss",
+      buildPath: "output/web/",
+      transforms: ["custom/flatten"],
+      files: [
+        {
+          destination: "_tokens.scss",
+          format: "scss/variables",
+        },
+      ],
+    },
+    ios: {
+      transformGroup: "ios",
+      buildPath: "output/ios/",
+      transforms: ["custom/flatten"],
+      files: [
+        {
+          destination: "Tokens.swift",
+          format: "ios/colors.swift",
+        },
+      ],
+    },
+    android: {
+      transformGroup: "android",
+      buildPath: "output/android/",
+      transforms: ["custom/flatten"],
+      files: [
+        {
+          destination: "Tokens.kt",
+          format: "android/colors",
+        },
+      ],
+    },
+  },
+};
