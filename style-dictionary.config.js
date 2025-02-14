dedc96accb9f6c87ecca5d2fdaa81e4963d7b386
rafalwlodarek <61891339+rafalwlodarek@users.noreply.github.com>
2025-02-14 01:39:09 +0100

    Update style-dictionary.config.js

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,5 +1,37 @@
 const StyleDictionary = require('style-dictionary');
 
+StyleDictionary.registerTransform({
+  name: 'attribute/cti',
+  type: 'attribute',
+  transformer: function(token) {
+    return token.$value;
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: 'name/cti/pascal',
+  type: 'name',
+  transformer: function(token) {
+    return token.name.replace(/([a-z])([A-Z])/g, '$1-$2').toUpperCase();
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: 'color/UIColor',
+  type: 'value',
+  transformer: function(token) {
+    return `UIColor(red: ${parseInt(token.value.slice(1, 3), 16) / 255.0}, green: ${parseInt(token.value.slice(3, 5), 16) / 255.0}, blue: ${parseInt(token.value.slice(5, 7), 16) / 255.0}, alpha: 1)`;
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: 'content/objC/literal',
+  type: 'value',
+  transformer: function(token) {
+    return token.$value;
+  }
+});
+
 // ✅ Register a custom format for Compose (Jetpack Compose)
 StyleDictionary.registerFormat({
   name: 'compose-kotlin',
